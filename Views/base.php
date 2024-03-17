433cc804435ec937b61e35ce8ea152a7e6c5ca36
= <bendev4977@gmail.com>
2024-03-17 23:58:29 +0100

    update

--- Views/base.php
+++ Views/base.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PlaygroundPOO</title>
+    <title>PlaygroundPOO <?= $title ?? ""; ?></title>
 </head>
 <body>
-    <?= $content ?>
+
+<?php require_once "_partials/_header.php"; ?>
+<main><?= $content ?></main>
+
 </body>
 </html>
---
+++ Views/main/index.php
@@ -0,0 +1,1 @@
+<?php $titre = "Page d'accueil"; ?>
