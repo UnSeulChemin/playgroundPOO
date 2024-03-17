7e16b186cc45b919452785e02dfb3a46746f2c3e
= <bendev4977@gmail.com>
2024-03-18 00:17:01 +0100

    update

--- Views/_partials/_header.php
+++ Views/_partials/_header.php
@@ -1,7 +1,12 @@
 <header>
-    <nav>
+    <nav class="flex-between">
         <div>
-            <a href="">Home</a>
+            <a class="link-menu" href="./">Home</a>
         </div>
+
+        <ul class="flex">
+            <li class="margin-right-li"><a class="link-menu" href="./">Register</a></li>
+            <li><a class="link-menu" href="./">Login</a></li>
+        </ul>
     </nav>
 </header>
