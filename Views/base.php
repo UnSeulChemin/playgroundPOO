e8ae924e53a45e1877992be8723e15c28530f549
= <bendev4977@gmail.com>
2024-03-18 00:08:29 +0100

    update

--- Views/base.php
+++ Views/base.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PlaygroundPOO <?= $title ?? ""; ?></title>
+	<link rel="shortcut icon" type="image/png" href="public/images/favicon/favicon.png">
+	<link rel="stylesheet" type="text/css" href="public/css/reset.css">
+	<link rel="stylesheet" type="text/css" href="public/css/app.css">
 </head>
 <body>
 
