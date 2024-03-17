7d0a1a5c9199cab9b38d7372376792e4a3388560
= <bendev4977@gmail.com>
2024-03-17 23:54:44 +0100

    update

--- Core/Main.php
+++ Core/Main.php
@@ -8,7 +8,7 @@ class Main
 {
     public function start()
     {
-        session_start();
+        // session_start();
 
         $uri = $_SERVER['REQUEST_URI'];
 
@@ -30,18 +30,14 @@ class Main
 
         if($params[0] != "")
         {
-            // On sauvegarde le 1er paramètre dans $controller en mettant sa 1ère lettre en majuscule, en ajoutant le namespace des controleurs et en ajoutant "Controller" à la fin
             $controller = '\\App\\Controllers\\'.ucfirst(array_shift($params)).'Controller';
 
-            // On sauvegarde le 2ème paramètre dans $action si il existe, sinon index
             $action = isset($params[0]) ? array_shift($params) : 'index';
 
-            // On instancie le contrôleur
             $controller = new $controller();
 
             if(method_exists($controller, $action))
             {
-                // Si il reste des paramètres, on appelle la méthode en envoyant les paramètres sinon on l'appelle "à vide"
                 (isset($params[0])) ? call_user_func_array([$controller,$action], $params) : $controller->$action();
             }
             
@@ -54,11 +50,7 @@ class Main
         
         else
         {
-            // Ici aucun paramètre n'est défini
-            // On instancie le contrôleur par défaut (page d'accueil)
             $controller = new MainController();
-
-            // On appelle la méthode index
             $controller->index();
         }
     }
