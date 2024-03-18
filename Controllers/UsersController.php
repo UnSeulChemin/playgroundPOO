e0c1e962f819e774887844c0a1d6781ebfc243ca
= <bendev4977@gmail.com>
2024-03-18 02:16:03 +0100

    update

--- Controllers/UsersController.php
+++ Controllers/UsersController.php
@@ -7,6 +7,36 @@ use App\Models\UsersModel;
 
 class UsersController extends Controller
 {
+    public function register()
+    {
+        if(Form::validate($_POST, ['email', 'password']))
+        {
+            $email = strip_tags($_POST['email']);
+            $pass = password_hash($_POST['password'], PASSWORD_ARGON2I);
+
+            $user = new UsersModel;
+            $user->setEmail($email)
+                ->setPassword($pass);
+            $user->create();
+        }
+
+        $form = new Form;
+
+        $form->startForm()
+            ->addLabelFor('email', 'E-mail :')
+            ->addInput('email', 'email', ['class' => 'form-control', 'id' => 'email'])
+            ->addLabelFor('pass', 'Mot de passe :')
+            ->addInput('password', 'password', ['id' => 'pass', 'class' => 'form-control'])
+            ->addBouton('M\'inscrire', ['class' => 'btn btn-primary'])
+            ->endForm();
+
+        $this->render('users/register', ['registerForm' => $form->create()]);
+    }
+
+
+
+
+
     public function login()
     {
         if(Form::validate($_POST, ['email', 'password']))
@@ -39,41 +69,15 @@ class UsersController extends Controller
 
         $form = new Form;
 
-        $form->debutForm()
-            ->ajoutLabelFor('email', 'Email')
-            ->ajoutInput('email', 'email', ['id' => 'email', 'class' => 'form-control'])
-            ->ajoutLabelFor('password', 'Mot de passe')
-            ->ajoutInput('password', 'password', ['id' => 'password', 'class' => 'form-control'])
-            ->ajoutBouton('Me connecter', ['class' => 'btn btn-primary'])
-            ->finForm();
+        $form->startForm()
+            ->addLabelFor('email', 'Email')
+            ->addInput('email', 'email', ['id' => 'email', 'class' => 'form-control'])
+            ->addLabelFor('password', 'Mot de passe')
+            ->addInput('password', 'password', ['id' => 'password', 'class' => 'form-control'])
+            ->addBouton('Me connecter', ['class' => 'btn btn-primary'])
+            ->endForm();
 
         $this->render('users/login', ['loginForm' => $form->create()]);
-    }
-
-    public function register()
-    {
-        if(Form::validate($_POST, ['email', 'password']))
-        {
-            $email = strip_tags($_POST['email']);
-            $pass = password_hash($_POST['password'], PASSWORD_ARGON2I);
-
-            $user = new UsersModel;
-            $user->setEmail($email)
-                ->setPassword($pass);
-            $user->create();
-        }
-
-        $form = new Form;
-
-        $form->debutForm()
-            ->ajoutLabelFor('email', 'E-mail :')
-            ->ajoutInput('email', 'email', ['class' => 'form-control', 'id' => 'email'])
-            ->ajoutLabelFor('pass', 'Mot de passe :')
-            ->ajoutInput('password', 'password', ['id' => 'pass', 'class' => 'form-control'])
-            ->ajoutBouton('M\'inscrire', ['class' => 'btn btn-primary'])
-            ->finForm();
-
-        $this->render('users/register', ['registerForm' => $form->create()]);
     }
 
     public function logout()
--- Core/Form.php
+++ Core/Form.php
@@ -20,73 +20,73 @@ class Form
                 return false;
             }
         }
-
+    
         return true;
     }
 
-    private function ajoutAttributs(array $attributs): string
+    private function addAttributes(array $attributes): string
     {
         $str = '';
 
-        $courts = ['checked', 'disabled', 'readonly', 'multiple', 'required', 'autofocus', 'novalidate', 'formnovalidate'];
+        $shorts = ['checked', 'disabled', 'readonly', 'multiple', 'required', 'autofocus', 'novalidate', 'formnovalidate'];
 
-        foreach($attributs as $attribut => $valeur)
+        foreach($attributes as $attribute => $valeur)
         {
-            if(in_array($attribut, $courts) && $valeur == true)
+            if(in_array($attribute, $shorts) && $valeur == true)
             {
-                $str .= " $attribut";
+                $str .= " $attribute";
             }
             
             else
             {
-                $str .= " $attribut=\"$valeur\"";
+                $str .= " $attribute=\"$valeur\"";
             }
         }
 
         return $str;
     }
 
-    public function debutForm(string $methode = 'post', string $action = '#', array $attributs = []): self
+    public function startForm(string $methode = 'post', string $action = '#', array $attributs = []): self
     {
         $this->formCode .= "<form action='$action' method='$methode'";
-        $this->formCode .= $attributs ? $this->ajoutAttributs($attributs).'>' : '>';
+        $this->formCode .= $attributs ? $this->addAttributes($attributs).'>' : '>';
         return $this;
     }
 
-    public function finForm():self
+    public function endForm():self
     {
         $this->formCode .= '</form>';
         return $this;
     }
 
-    public function ajoutLabelFor(string $for, string $texte, array $attributs = []):self
+    public function addLabelFor(string $for, string $texte, array $attributs = []):self
     {
         $this->formCode .= "<label for='$for'";
-        $this->formCode .= $attributs ? $this->ajoutAttributs($attributs) : '';
+        $this->formCode .= $attributs ? $this->addAttributes($attributs) : '';
         $this->formCode .= ">$texte</label>";
 
         return $this;
     }
 
-    public function ajoutInput(string $type, string $nom, array $attributs = []): self
+    public function addInput(string $type, string $nom, array $attributs = []): self
     {
         $this->formCode .= "<input type='$type' name='$nom'";
-        $this->formCode .= $attributs ? $this->ajoutAttributs($attributs).'>' : '>';
+        $this->formCode .= $attributs ? $this->addAttributes($attributs).'>' : '>';
         return $this;
     }
 
-    public function ajoutTextarea(string $nom, string $valeur = '', array $attributs = []):self
+    public function addTextarea(string $nom, string $valeur = '', array $attributs = []):self
     {
         $this->formCode .= "<textarea name='$nom'";
-        $this->formCode .= $attributs ? $this->ajoutAttributs($attributs) : '';
+        $this->formCode .= $attributs ? $this->addAttributes($attributs) : '';
         $this->formCode .= ">$valeur</textarea>";
         return $this;
     }
 
-    public function ajoutSelect(string $nom, array $options, array $attributs = []):self
+    public function addSelect(string $nom, array $options, array $attributs = []):self
     {
         $this->formCode .= "<select name='$nom'";
-        $this->formCode .= $attributs ? $this->ajoutAttributs($attributs).'>' : '>';
+        $this->formCode .= $attributs ? $this->addAttributes($attributs).'>' : '>';
 
         foreach($options as $valeur => $texte)
         {
@@ -97,12 +97,11 @@ class Form
         return $this;
     }
 
-    public function ajoutBouton(string $texte, array $attributs = []): self
+    public function addBouton(string $texte, array $attributs = []): self
     {
         $this->formCode .= '<button ';
-        $this->formCode .= $attributs ? $this->ajoutAttributs($attributs) : '';
+        $this->formCode .= $attributs ? $this->addAttributes($attributs) : '';
         $this->formCode .= ">$texte</button>";
-
         return $this;
     }
 }
