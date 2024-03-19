2a947699610d20ac4bbdee390edf6bf102f97cdc
= <bendev4977@gmail.com>
2024-03-19 22:39:09 +0100

    update

--- Controllers/ShipsController.php
+++ Controllers/ShipsController.php
@@ -1,25 +1,44 @@
 <?php
 namespace App\Controllers;
 
+use App\Core\Functions;
 use App\Models\ShipsModel;
 
 class ShipsController extends Controller
 {
     public function index()
     {
-        $shipModel = new ShipsModel;
-        $ships = $shipModel->findAll();
+        if (Functions::sessionUser())
+        {
+            $shipModel = new ShipsModel;
+            $ships = $shipModel->findAll();
+    
+            $this->title = 'PlaygroundPOO | Ships';
+            $this->render("ships/index", ["ships" => $ships]);
+        }
 
-        $this->title = 'PlaygroundPOO | Ships';
-        $this->render("ships/index", ["ships" => $ships]);
+        else
+        {
+            header('Location: users/login');
+            exit;
+        }
     }
 
     public function show(int $id)
     {
-        $shipModel = new ShipsModel;
-        $ship = $shipModel->find($id);
+        if (Functions::sessionUser())
+        {
+            $shipModel = new ShipsModel;
+            $ship = $shipModel->find($id);
+    
+            $this->title = 'PlaygroundPOO | '.$ship->name;
+            $this->render('ships/show', ["ship" => $ship]);
+        }
 
-        $this->title = 'PlaygroundPOO | '.$ship->name;
-        $this->render('ships/show', ["ship" => $ship]);
+        else
+        {
+            header('Location: ../users/login');
+            exit;
+        }
     }
 }
--- Controllers/UsersController.php
+++ Controllers/UsersController.php
@@ -9,63 +9,80 @@ class UsersController extends Controller
 {
     public function register()
     {
-        if (Form::validate($_POST, ['email', 'password']))
+        if (Functions::sessionEmpty())
         {
-            $email = strip_tags($_POST['email']);
-            $password = password_hash($_POST['password'], PASSWORD_ARGON2I);
-            $roles = ['ROLE_USER'];
-
-            $userModel = new UsersModel;
-            $userModel->setEmail($email)->setPassword($password)->setRoles($roles);
-
-            if ($userModel->create())
+            if (Form::validate($_POST, ['email', 'password']))
             {
-                $userArray = $userModel->findOneByEmail($email);
-                $user = $userModel->hydrate($userArray);
-                $user->setSession();
-                header("Location: .././");
-                exit;
+                $email = strip_tags($_POST['email']);
+                $password = password_hash($_POST['password'], PASSWORD_ARGON2I);
+                $roles = ['ROLE_USER'];
+    
+                $userModel = new UsersModel;
+                $userModel->setEmail($email)->setPassword($password)->setRoles($roles);
+    
+                if ($userModel->create())
+                {
+                    $userArray = $userModel->findOneByEmail($email);
+                    $user = $userModel->hydrate($userArray);
+                    $user->setSession();
+                    header("Location: .././");
+                    exit;
+                }
             }
+    
+            else
+            {
+                $_SESSION['warning'] = !empty($_POST) ? "Form is empty." : '';
+                $email = isset($_POST['email']) ? strip_tags($_POST['email']) : '';
+                $password = isset($_POST['password']) ? strip_tags($_POST['password']) : '';
+            }
+    
+            $form = new Form;
+    
+            $form->startForm()
+                ->startDiv()->addInput('email', 'email', ['placeholder' => 'Email', 'autofocus' => true])->endDiv()
+                ->startDiv()->addInput('password', 'password', ['placeholder' => 'Password'])->endDiv()
+                ->addButton('Register', ['type' => 'submit', 'class' => 'link-form'])
+                ->endForm();
+    
+            $this->title = 'PlaygroundPOO | Register';
+            $this->render('users/register', ['registerForm' => $form->create()]);
         }
 
         else
         {
-            $_SESSION['warning'] = !empty($_POST) ? "Form is empty." : '';
-            $email = isset($_POST['email']) ? strip_tags($_POST['email']) : '';
-            $password = isset($_POST['password']) ? strip_tags($_POST['password']) : '';
+            header('Location: '.Functions::pathRedirect().'./');
+            exit;
         }
-
-        $form = new Form;
-
-        $form->startForm()
-            ->startDiv()->addInput('email', 'email', ['placeholder' => 'Email', 'autofocus' => true])->endDiv()
-            ->startDiv()->addInput('password', 'password', ['placeholder' => 'Password'])->endDiv()
-            ->addButton('Register', ['type' => 'submit', 'class' => 'link-form'])
-            ->endForm();
-
-        $this->title = 'PlaygroundPOO | Register';
-        $this->render('users/register', ['registerForm' => $form->create()]);
     }
 
     public function login()
     {
-        if (Form::validate($_POST, ['email', 'password']))
+        if (Functions::sessionEmpty())
         {
-            $email = strip_tags($_POST['email']);
-            $password = strip_tags($_POST['password']);
-
-            $userModel = new UsersModel;
-            $userArray = $userModel->findOneByEmail($email);
-
-            if ($userArray)
+            if (Form::validate($_POST, ['email', 'password']))
             {
-                $user = $userModel->hydrate($userArray);
-
-                if (password_verify($password, $user->getPassword()))
+                $email = strip_tags($_POST['email']);
+                $password = strip_tags($_POST['password']);
+    
+                $userModel = new UsersModel;
+                $userArray = $userModel->findOneByEmail($email);
+    
+                if ($userArray)
                 {
-                    $user->setSession();
-                    header("Location: .././");
-                    exit;
+                    $user = $userModel->hydrate($userArray);
+    
+                    if (password_verify($password, $user->getPassword()))
+                    {
+                        $user->setSession();
+                        header("Location: .././");
+                        exit;
+                    }
+        
+                    else
+                    {
+                        $_SESSION["warning"] = "Email and / or password is incorrect.";
+                    }
                 }
     
                 else
@@ -73,30 +90,31 @@ class UsersController extends Controller
                     $_SESSION["warning"] = "Email and / or password is incorrect.";
                 }
             }
-
+    
             else
             {
-                $_SESSION["warning"] = "Email and / or password is incorrect.";
+                $_SESSION['warning'] = !empty($_POST) ? "Form is empty." : '';
+                $email = isset($_POST['email']) ? strip_tags($_POST['email']) : '';
+                $password = isset($_POST['password']) ? strip_tags($_POST['password']) : '';
             }
+    
+            $form = new Form;
+    
+            $form->startForm()
+                ->startDiv()->addInput('email', 'email', ['placeholder' => 'Email', 'autofocus' => true])->endDiv()
+                ->startDiv()->addInput('password', 'password', ['placeholder' => 'Password'])->endDiv()
+                ->addButton('Login', ['type' => 'submit', 'class' => 'link-form'])
+                ->endForm();
+    
+            $this->title = 'PlaygroundPOO | Login';
+            $this->render('users/login', ['loginForm' => $form->create()]);
         }
 
         else
         {
-            $_SESSION['warning'] = !empty($_POST) ? "Form is empty." : '';
-            $email = isset($_POST['email']) ? strip_tags($_POST['email']) : '';
-            $password = isset($_POST['password']) ? strip_tags($_POST['password']) : '';
+            header('Location: '.Functions::pathRedirect().'./');
+            exit;
         }
-
-        $form = new Form;
-
-        $form->startForm()
-            ->startDiv()->addInput('email', 'email', ['placeholder' => 'Email', 'autofocus' => true])->endDiv()
-            ->startDiv()->addInput('password', 'password', ['placeholder' => 'Password'])->endDiv()
-            ->addButton('Login', ['type' => 'submit', 'class' => 'link-form'])
-            ->endForm();
-
-        $this->title = 'PlaygroundPOO | Login';
-        $this->render('users/login', ['loginForm' => $form->create()]);
     }
 
     public function logout()
