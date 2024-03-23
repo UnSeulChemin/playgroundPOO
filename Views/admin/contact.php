8254606d6cc331f07a90ab69d9adf75ce102b9ca
= <bendev4977@gmail.com>
2024-03-23 05:04:07 +0100

    update

--- Views/admin/contact.php
+++ Views/admin/contact.php
@@ -3,9 +3,18 @@
     <h2>Contact <?= $contact->id ?></h2>
    
     <article class="article-content">
-        <p class="bold"><?= $contact->title ?></p>
-        <p><?= $contact->description ?></p>
-        <p><?= date('d/m/Y', strtotime($contact->created_at)); ?></p>
+        <div>
+            <p class="bold"><?= $contact->title ?></p>
+        </div>
+        <div>
+            <p><?= $contact->description ?></p>
+        </div>
+        <div>
+            <p><?= date('d/m/Y', strtotime($contact->created_at)); ?></p>
+        </div>
+
+
+
         <a class="link-form" href="javascript:history.go(-1)">Back</a>
     </article>
 
