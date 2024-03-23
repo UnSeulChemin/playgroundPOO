f9c6564bcbb27d2381eed5462a638e5ed3b45c14
= <bendev4977@gmail.com>
2024-03-23 05:09:12 +0100

    update

--- Views/admin/contact.php
+++ Views/admin/contact.php
@@ -12,9 +12,6 @@
         <div>
             <p><?= date('d/m/Y', strtotime($contact->created_at)); ?></p>
         </div>
-
-
-
         <a class="link-form" href="javascript:history.go(-1)">Back</a>
     </article>
 
